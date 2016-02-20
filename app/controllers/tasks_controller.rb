f5fcb04b893405222e4b42a8922bbf84617a5c0c
OlegKras <oleg_krasovskiy@mail.ua>
2016-02-20 16:46:13 +0200

    Start

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,16 +1,24 @@
 class TasksController < ApplicationController
+	
 	def create
 	  @project = Project.find(params[:project_id])	
 	  @task = @project.tasks.create(task_params)
 	  redirect_to project_path(@project)
 	end
 
+	def update
+	  @project = Project.find(params[:project_id])	
+	  @task = @project.tasks.update(task_params)
+	  redirect_to project_path(@project)
+	end
+
+
 	def destroy
-    @project = Project.find(params[:project_id])
-    @task = @project.tasks.find(params[:id])
-    @task.destroy
-    redirect_to projects_path
-  end
+      @project = Project.find(params[:project_id])
+      @task = @project.tasks.find(params[:id])
+      @task.destroy
+      redirect_to projects_path
+    end
 
 	private
 	  def task_params
