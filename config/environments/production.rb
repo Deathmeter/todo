a4a1b87a348d112e8784c501731ab1fffe3f56bb
OlegKras <oleg_krasovskiy@mail.ua>
2016-02-28 18:51:38 +0200

    ver2.2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,5 +77,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
-  config.action_mailer.default_url_options = { :host => 'yourapp.heroku.com' }
+  config.action_mailer.default_url_options = { :host => 'https://still-retreat-14450.herokuapp.com' }
 end
